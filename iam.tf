457c3da86f8cb27e5b24a893a3420d62c4f1bd91
Shriniwas Prachand <144546134+Shriniwas27@users.noreply.github.com>
2025-08-03 16:14:29 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -6,11 +6,11 @@
 # without needing hardcoded credentials.
 # =================================================================================
 
-# Create an IAM Role that EC2 instances can assume.
+
 resource "aws_iam_role" "ec2_role" {
   name = "fastapi-ec2-role"
 
-  # This policy allows EC2 instances to assume this role.
+
   assume_role_policy = jsonencode({
     Version   = "2012-10-17",
     Statement = [
@@ -29,8 +29,7 @@ resource "aws_iam_role" "ec2_role" {
   }
 }
 
-# --- Policy for CloudWatch ---
-# This policy grants permission to create and write to log streams.
+
 resource "aws_iam_policy" "cloudwatch_policy" {
   name        = "fastapi-cloudwatch-policy"
   description = "Allows EC2 instances to push logs to CloudWatch"
@@ -52,15 +51,14 @@ resource "aws_iam_policy" "cloudwatch_policy" {
   })
 }
 
-# Attach the CloudWatch policy to our EC2 role.
+
 resource "aws_iam_role_policy_attachment" "cloudwatch" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.cloudwatch_policy.arn
 }
 
 
-# --- Policy for Secrets Manager ---
-# This policy grants permission to read the specific secret containing the DB password.
+
 resource "aws_iam_policy" "secrets_manager_policy" {
   name        = "fastapi-secrets-manager-policy"
   description = "Allows EC2 instances to read the DB password from Secrets Manager"
@@ -77,15 +75,14 @@ resource "aws_iam_policy" "secrets_manager_policy" {
   })
 }
 
-# Attach the Secrets Manager policy to our EC2 role.
+
 resource "aws_iam_role_policy_attachment" "secrets_manager" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.secrets_manager_policy.arn
 }
 
 
-# --- Instance Profile ---
-# The instance profile is a container for the IAM role that we can attach to an EC2 instance.
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "fastapi-ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
